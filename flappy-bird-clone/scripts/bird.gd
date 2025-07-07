551111f58fa04c4436c5f7dca27be26a347cd14c
Stark <atipvietnghiem@gmail.com>
2025-07-07 20:00:13 +0200

    Redoing Flappy Bird(learned new things)

--- flappy-bird-clone/scripts/bird.gd
+++ flappy-bird-clone/scripts/bird.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-const GRAVITY = 1000
+signal hit
+
+var speed := 500
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
 
 func _physics_process(delta: float) -> void:
-	velocity.y += GRAVITY * delta
-	get_input()
+	velocity.y += gravity * delta
+	fly()
 	move_and_slide()
 
-func get_input():
+func fly():
 	if Input.is_action_just_pressed("UP"):
-		velocity.y = -500
-
-
+		velocity.y = -speed 
+	
+	
--- flappy-bird-clone/scripts/world.gd
+++ flappy-bird-clone/scripts/world.gd
@@ -1,22 +1,9 @@
 extends Node2D
 
-@export var pipe_scene: PackedScene
-@export var spawn_interval := 1.5  # seconds
 
-var timer := 0.0
+func _ready():
+	new_game()
 
-func _process(delta: float) -> void:
-	timer -= delta
-	if timer <= 0.0:
-		_spawn_pipe()
-		timer = spawn_interval
-
-func _spawn_pipe() -> void:
-	var pipe = pipe_scene.instantiate()
-	pipe.position = Vector2(get_viewport_rect().size.x  + 75, 568)  # spawn off-screen to right
-	add_child(pipe)
-
-func player_oob():
-	if $Bird.position.x < -20:
-		queue_free()
-		print("Bird dead")
+func new_game():
+	$Bird.position = $StartPos.position
+	$Floor.position = $FloorPos.position
