269c097bde088a13346fdacb0c7e20880cbae6ef
Stark <atipvietnghiem@gmail.com>
2025-07-08 12:33:41 +0200

    Updated

--- flappy-bird-clone/scripts/world.gd
+++ flappy-bird-clone/scripts/world.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var pipe_scene: PackedScene
 
 func _ready():
 	new_game()
+	spawn_pipes()
 
 func new_game():
 	$Bird.position = $StartPos.position
 	$Floor.position = $FloorPos.position
+
+func spawn_pipes():
+	var pipe = pipe_scene.instantiate()
+	$pipes.postition.x = 500
+	add_child(pipe)
